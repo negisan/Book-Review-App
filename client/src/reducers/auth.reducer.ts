dd889fd72fd22991ac4094561a923e1ae9c1df0f
wayne <juchuang123@gmail.com>
2021-09-23 17:45:17 +0900

    authreducerにエラー処理の追記

--- client/src/reducers/auth.reducer.ts
+++ client/src/reducers/auth.reducer.ts
@@ -23,5 +23,6 @@ const auth_reducer = (state, action) => {
   if (action.type === FETCH_USER_FAIL) {
     return { ...state, user_error: action.payload }
   }
+  throw new Error(`No Matching "${action.type}" - action type`)
 }
 export default auth_reducer
