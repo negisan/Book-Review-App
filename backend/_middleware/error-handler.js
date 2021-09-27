a3b774ee9a26a76a3924004dc14cf2ef56dbf079
wayne <juchuang123@gmail.com>
2021-09-27 21:13:05 +0900

    エラーハンドラーの実装

---
+++ backend/_middleware/error-handler.js
@@ -0,0 +1,30 @@
+module.exports = errorHandler
+
+function errorHandler(err, req, res, next) {
+  console.log('errHandler==========', err)
+  switch (true) {
+    case typeof err === 'string':
+      const is404 = err.toLowerCase().endsWith('not found')
+      const statusCode = is404 ? 404 : 400
+      if (is404) {
+        return res.status(statusCode).json({
+          ErrorMessageJP: 'その書籍はまだレビューされていません。',
+          ErrorMessageEN: 'Error occured at server',
+        })
+      } else {
+        res.status(statusCode).json({
+          ErrorMessageJP: 'バリデーションエラー',
+          ErrorMessageEN: 'validation error',
+        })
+      }
+    case err.name === 'UnauthorizedError':
+      return res
+        .status(401)
+        .json({ ErrorMessageJP: '認証エラー', ErrorMessageEN: 'Unautorized' })
+    default:
+      return res.status(500).json({
+        ErrorMessageJP: 'サーバーでエラーが発生しました。',
+        ErrorMessageEN: 'Error occured at server',
+      })
+  }
+}
---
+++ backend/server.js
@@ -0,0 +1,20 @@
+require('rootpath')()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const errorHandler = require('_middleware/error-handler')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.use(errorHandler)
+
+const port =
+  process.env.NODE_ENV === 'production' ? process.env.PORT || 80 : 4000
+app.listen(port, () => console.log('Server listening on port ' + port))
