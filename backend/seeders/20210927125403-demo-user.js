02255e7b7992d542dac9a5b425a88bc4df90cae0
wayne <juchuang123@gmail.com>
2021-09-28 00:01:28 +0900

    demoUserSeedの作成

---
+++ backend/seeders/20210927125403-demo-user.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const bcrypt = require('bcrypt')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    return queryInterface.bulkInsert('Users', [
+      {
+        name: 'string',
+        email: 'string@email.com',
+        hash: await bcrypt.hash('string', 10),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return queryInterface.bulkDelete('Users', null, {})
+  },
+}
