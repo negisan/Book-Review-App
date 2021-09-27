e01bd427fe1b864ee1b65337e2b04dc69df5de1e
wayne <juchuang123@gmail.com>
2021-09-27 23:00:08 +0900

    migration-create-userでUsersTableのemailカラムにユニーク制限

--- backend/migrations/20210927082118-create-user.js
+++ backend/migrations/20210927082118-create-user.js
@@ -1,34 +1,44 @@
 'use strict'
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
+    await queryInterface.createTable(
+      'Users',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        email: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        hash: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
       },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      hash: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    })
+      {
+        uniqueKeys: {
+          Items_unique: {
+            fields: ['email'],
+          },
+        },
+      }
+    )
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('Users')
