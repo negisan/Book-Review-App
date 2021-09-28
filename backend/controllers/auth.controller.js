87f11efe98c80a4cea003e969f01ea40ec635e22
wayne <juchuang123@gmail.com>
2021-09-28 17:12:00 +0900

    publicControllerのgetBooksの処理を修正

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -10,7 +10,6 @@ router.post('/signin', authenticateSchema, authenticate)
 module.exports = router
 
 function authenticate(req, res, next) {
-  console.log('controller==========', req.body)
   userService
     .authenticate(req.body)
     .then((user) => {
--- backend/services/public.service.js
+++ backend/services/public.service.js
@@ -6,15 +6,43 @@ module.exports = {
 
 async function getBooks(query) {
   const limit = 10
+  let booksWithUser = []
 
   if (query.offset) {
-    return await db.Book.findAndCountAll({
+    booksWithUser = await db.Book.findAndCountAll({
       offset: parseInt(query.offset, 10),
       limit: limit,
+      include: [
+        {
+          model: db.User,
+        },
+      ],
+    })
+      .then((res) => {
+        return res.rows
+      })
+      .catch((e) => console.log(e))
+  } else {
+    booksWithUser = await db.Book.findAll({
+      limit: limit,
+      include: [
+        {
+          model: db.User,
+        },
+      ],
     })
   }
 
-  return await db.Book.findAll({
-    limit: limit,
+  // data shaping
+  const res = booksWithUser.map((book) => {
+    return {
+      id: book.uuid,
+      title: book.title,
+      url: book.url,
+      detail: book.detail,
+      review: book.review,
+      reviewer: book.User.name,
+    }
   })
+  return res
 }
