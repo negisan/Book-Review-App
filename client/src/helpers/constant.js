15bfb1f1356f31c524bdd58368088ca98df4efe4
wayne <juchuang123@gmail.com>
2021-09-28 15:30:09 +0900

    デフォルトのBASE_API_URLを変更

--- client/src/helpers/constant.js
+++ client/src/helpers/constant.js
@@ -1,3 +1,3 @@
-// export const BASE_API_URL =
-//   'https://api-for-missions-and-railways.herokuapp.com'
-export const BASE_API_URL = 'http://localhost:4000'
+export const BASE_API_URL =
+  'https://api-for-missions-and-railways.herokuapp.com'
+// export const BASE_API_URL = 'http://localhost:4000'
